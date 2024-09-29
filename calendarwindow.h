640cf996af60c2b3c4020ff7640a3617996dee7d
khumnath <nath.khum@gmail.com>
2024-09-30 07:15:45 +0900

    applicatoion update
    code refactor/
    added month navigation button/
    bumped app version to 2.0.0/

--- calendarwindow.h
+++ calendarwindow.h
@@ -1,13 +1,13 @@
 #ifndef CALENDARWINDOW_H
 #define CALENDARWINDOW_H
-#include <QMainWindow>
 
+#include <QMainWindow>
 #include "bikram.h"
+#include "qdatetime.h"
 #include <QMessageBox>
 #include <QMenu>
 #include <QDesktopServices>
 #include <QUrl>
-
 
 namespace Ui {
 class CalendarWindow;
@@ -18,54 +18,19 @@ class CalendarWindow : public QMainWindow
     Q_OBJECT
 
 public:
-
-    int gYear, gMonth, gDay;
     explicit CalendarWindow(QWidget *parent = nullptr);
-    ~CalendarWindow(); 
-    QString getBikramMonthName(int month) {
-        QStringList bikramMonths = {"बैशाख", "जेठ", "असार", "श्रावण", "भाद्र",
-                                     "आश्विन", "कार्तिक", "मंसिर", "पौष", "माघ",
-                                     "फाल्गुन", "चैत"};
-        if (month >= 1 && month <= 12) {
-            return bikramMonths[month - 1];
-        } else {
-            return QString("Invalid Month");
-        }
-    }
-    QString getEnglishMonthName(int month) {
-        QStringList englishMonths = {"जनवरी", "फेब्रुअरी", "मार्च", "अप्रिल", "मे", "जुन",
-                                     "जुलाई", "अगस्ट", "सेप्टेम्बर", "अक्टोबर", "नोभेम्बर", "डिसेम्बर"};
+    ~CalendarWindow();
 
-        // Adjust month to use as an index (0-based in QStringList)
-        int index = month - 1;
+    // Static constant month names for Gregorian and Bikram Sambat calendars
+    static const QStringList gregorianMonths;
+    static const QStringList bikramMonths;
 
-        if (index >= 0 && index < englishMonths.size()) {
-            return englishMonths.at(index);
-        }
-
-        return "";  // Return empty string if index is out of range
-    }
+    // Methods to get month names
+    QString getBikramMonthName(int month);
+    QString getEnglishMonthName(int month);
 
     // Function to convert number to Nepali numeral string
-    QString convertToNepaliNumerals(int number) {
-        QString nepaliNumbers[] = {"०", "१", "२", "३", "४", "५", "६", "७", "८", "९"};
-        QString result;
-        QString numStr = QString::number(number);
-
-        for (QChar ch : numStr) {
-            if (ch.isDigit()) {
-                int digit = ch.digitValue();
-                result += nepaliNumbers[digit];
-            } else {
-                result += ch;
-            }
-        }
-
-        return result;
-    }
-
-
-
+    QString convertToNepaliNumerals(int number);
 
 private slots:
     void onAdYearChanged(int index);
@@ -75,6 +40,8 @@ private slots:
     void onBsMonthChanged(int index);
     void onBsDayChanged(int index);
     void ontodayButtonclicked();
+    void onnextMonthButtonclicked();
+    void onpreviousMonthButtonclicked();
     void showMenu();
     void showAbout();
     void openSourceCode();
@@ -87,6 +54,10 @@ private:
     Ui::CalendarWindow *ui;
     bikram converter;
     bool blockSignals;
+    int gYear, gMonth, gDay;  // Moved to private section for encapsulation
+    QDate currentBikramDate;
+
+    // Private helper methods
     void centerOnScreen();
     void updateBsDateFromAd(int year, int month, int day);
     void updateAdDateFromBs(int year, int month, int day);
@@ -96,7 +67,5 @@ private:
     void adjustCalendarTableSize();
     void populateBsDays(int year, int month);
 };
-
-
 
 #endif // CALENDARWINDOW_H
