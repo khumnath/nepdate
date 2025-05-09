860169c3383f89d02e9ba2e13fea92b60a5f61a0
khumnath <nath.khum@gmail.com>
2025-05-10 07:50:03 +0900

    possible fix for page

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-<<<<<<< HEAD
   base: mode === "ghpages" ? "nepdate/" : "/",
-=======
->>>>>>> 7912cbbd8047db138868214562356389812b0c13
   build: {
     outDir: mode === "ghpages" ? "docs" : "dist",
     emptyOutDir: true,
