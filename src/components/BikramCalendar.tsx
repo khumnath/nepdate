23264080533c8eb914cf6fa63b8f966fc29ffb8e
khumnath <nath.khum@gmail.com>
2025-05-10 11:17:40 +0900

    test

--- src/components/BikramCalendar.tsx
+++ src/components/BikramCalendar.tsx
@@ -82,7 +82,10 @@ const BikramCalendar: React.FC = () => {
     }
   }, [calendarState]);
   return (
-    <div className="min-h-screen bg-[url('$assets/subtle-pattern.png')] pt-2 md:pt-4">
+    <div
+  className="min-h-screen pt-2 md:pt-4"
+  style={{ backgroundImage: "url('./subtle-pattern.png')" }}
+>
       {/* Calendar Container */}
       <div className="mx-auto px-2 sm:px-4 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-4xl">
         {/* Top header with month/year */}
