13f6024bfe03c9feac5fbd50ecf4dd4d78ba96f5
khumnath <nath.khum@gmail.com>
2025-05-10 10:34:38 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { HashRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter basename="/nepdate">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig(({ mode }) => ({
   // fixme here: does not work on ghpages
-  base: mode === "ghpages" ? "/nepdate/" : "/",
+  base: mode === "ghpages" ? "./" : "/",
   build: {
     outDir: mode === "ghpages" ? "docs" : "dist",
     emptyOutDir: true,
